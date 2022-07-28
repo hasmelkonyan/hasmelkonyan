a275d78796e33ea91784b2674fa0287227141c3c
Hasmik Melkonyan <hasmelkonyan@gmail.com>
2022-07-28 12:52:21 +0400

    I added some functions

--- snake_game.py
+++ snake_game.py
@@ -2,9 +2,9 @@ import turtle
 import time
 import random
 
-score = 0
 highest_score = 0
 delay_time = 0.4
+snake_body = []
 
 wind = turtle.Screen()
 wind.title("Snake Game")
@@ -27,9 +27,8 @@ food.goto(0, 100)
 
 pen = turtle.Turtle()
 pen.penup()
-pen.color("white")
+pen.color("black")
 pen.goto(0, 250)
-pen.write(f"Score : {score}   Highest_Score : {highest_score}", align="center", font=("caddr", 20, "bold"))
 
 
 def go_up():
@@ -51,11 +50,11 @@ def go_right():
 def move():
     if snake_head.direction == "up":
         snake_head.sety(snake_head.ycor() + 20)
-    elif snake_head.direction == "down":
+    if snake_head.direction == "down":
         snake_head.sety(snake_head.ycor() - 20)
-    elif snake_head.direction == "right":
+    if snake_head.direction == "right":
         snake_head.setx(snake_head.xcor() + 20)
-    elif snake_head.direction == "left":
+    if snake_head.direction == "left":
         snake_head.setx(snake_head.xcor() - 20)
 
 
@@ -66,13 +65,54 @@ wind.onkeypress(go_left, "Left")
 wind.onkeypress(go_right, "Right")
 
 
-while True:
-    wind.update()
-    time.sleep(delay_time)
+def play():
+    score = 0
+    global highest_score
+    while True:
+        wind.update()
+        pen.write(f"Score : {score}   Highest_Score : {highest_score}", align="center", font=("caddr", 20, "bold"))
+        if snake_head.distance(food) < 20:
+            food.goto(random.randrange(-280, 280, 20), random.randrange(-280, 280, 20))
+            tail = turtle.Turtle()
+            tail.color("brown")
+            tail.shape("square")
+            tail.penup()
+            tail.speed(0)
+            snake_body.append(tail)
+            score += 1
 
-    if snake_head.distance(food) < 20:
-        food.goto(random.randrange(-280, 280, 20), random.randrange(-280, 280, 20))
+        for i in range(len(snake_body) - 1, -1, -1):
+            if i > 0:
+                snake_body[i].goto(snake_body[i - 1].xcor(), snake_body[i - 1].ycor())
+            else:
+                snake_body[0].goto(snake_head.xcor(), snake_head.ycor())
+        if snake_head.xcor() > 280 or snake_head.xcor() < -280 or snake_head.ycor() > 280 or snake_head.ycor() < -280:
+            time.sleep(1)
+            snake_head.goto(0, 0)
+            snake_body.clear()
+            food.goto(0, 100)
+            if highest_score < score:
+                highest_score = score
+                snake_head.direction = "stop"
+            play()
+
+        for each in snake_body:
+            if each.dictance(snake_head) < 20:
+                time.sleep(1)
+                snake_head.goto(0, 0)
+                snake_body.clear()
+                food.goto(0, 100)
+                if highest_score < score:
+                    highest_score = score
+                    snake_head.direction = "stop"
+            play()
 
 
-    move()
+        move()
+        time.sleep(delay_time)
+
+
+
+play()
+
 
